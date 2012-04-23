570193b2e0c0391620e79aeb02cb7df08538f023
Micah Gates <github@mgates.com>
2012-04-23 17:38:57 -0500

    added more questions

--- lib/job_interview/questions.rb
+++ lib/job_interview/questions.rb
@@ -9,7 +9,7 @@ module JobInterview
           "have made",
           "have enhanced shareholder value by creating",
           "made someone else rich with my"
-        ][rand(3) + 1] + " " +
+        ].sample + " " +
         Faker::Company.catch_phrase.downcase + "."
     end
 
@@ -20,15 +20,26 @@ module JobInterview
           "leaving because I can't" ,
           "leaving because I have to",
           "not happy with the oppertunities I have for"
-        ][rand(4) + 1] + " " +
+        ].sample + " " +
         Faker::Company.bs + "."
     end
 
     def manhole_cover
+      [
+        "",
+        "Because men are round",
+        "Because manholes are round",
+        "So they don't fall in",
+        "Because Reuleaux Triangles are hard to manufacture"
+      ].sample
 
     end
 
     def greatest_weakness
+      ["Some times I", "I always"].sample + " " +
+      ["try too hard", "work too much", "care too much", "fail so rarely"].sample  + " " +
+      "so I " +
+      ["make others jealous", "make too much money"].sample + "."
     end
   end
 end
