787bbea0130754d33cc6f6bd323403ca626a8565
Micah Gates <github@mgates.com>
2012-04-23 15:14:03 -0500

    merge

--- lib/job_interview.rb
+++ lib/job_interview.rb
@@ -3,14 +3,4 @@ require File.expand_path('../job_interview/fibonacci', __FILE__)
 require File.expand_path('../job_interview/knapsack' , __FILE__)
 require File.expand_path('../job_interview/answer' , __FILE__)
 module JobInterview
-<<<<<<< HEAD
- autoload :FizzBuzz, File.expand_path('../job_interview/fizz_buzz', __FILE__)
- autoload :Fibonacci, File.expand_path('../job_interview/fibonacci', __FILE__)
- autoload :Knapsack, File.expand_path('../job_interview/knapsack' , __FILE__)
-
 end
-=======
-
-  
-end
->>>>>>> 3befc29c610a69f56d136e9821294ff397ccdfcf
