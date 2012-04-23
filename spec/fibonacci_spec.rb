5f5b9a815f2f1095a2232fcf0b1d2022c13eec14
Micah Gates <github@mgates.com>
2012-04-23 15:15:20 -0500

    fib test

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper.rb"
 module FibonacciSpec
-  
+
 
 
   describe "When called" do
     before(:each) do
       @answer = JobInterview::Answer.new
     end
-    
+
     it "should return the fibinacci sequence" do
-      @answer.fib(10).should == [1, 1, 2, 3, 5, 8, 12, 20, 32, 52]
+      @answer.fib(10).should == 55
     end
   end
 
