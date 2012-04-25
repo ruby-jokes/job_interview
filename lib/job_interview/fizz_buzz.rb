f693ed2e0edcfc755aa88569991a931ca0a8e147
Micah Gates <github@mgates.com>
2012-04-25 10:06:50 -0500

    nicer fizzbuzz

--- lib/job_interview/fizz_buzz.rb
+++ lib/job_interview/fizz_buzz.rb
@@ -1,21 +1,13 @@
 module JobInterview
   module FizzBuzz
-    
+
     def fizz_buzz(max)
-      acc = []
-      (1..max).each do |n|
-        if ((n % 3 == 0) && (n % 5 == 0))
-          acc << "FizzBuzz"
-        elsif (n % 3 == 0)
-          acc << "Fizz"
-        elsif (n % 5 == 0)
-          acc << "Buzz"
-        else
-          acc << n
-        end
+      Array.new(max) do |i|
+        j = i + 1
+        val  = (j % 3 == 0 ? "Fizz" : "") +
+        (j % 5 == 0 ? "Buzz" : "")
+        val.empty? ?  j.to_s  : val
       end
-      return acc
     end
-    
   end
 end
